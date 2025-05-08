a289996e27e9f26cf1d4c14d44178b2d39347304
StewartNash <Stewart.M.Nash@gmail.com>
2025-05-08 00:15:09 -0400

    Fixed plots for ex 11-3

--- controls/examples_11_3_sensitivity.py
+++ controls/examples_11_3_sensitivity.py
@@ -17,26 +17,31 @@ P = control.TransferFunction([1, a], [1, b]) # P_lead
 compensated = P * GH
 
 omega = numpy.logspace(-2, 2, 500)
-_, _, GH_response = control.frequency_response(GH, omega)
-_, _, compensated_response = control.frequency_response(compensated, omega)
+GH_response = control.frequency_response(GH, omega)
+compensated_response = control.frequency_response(compensated, omega)
 
-fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 5))
+H = GH_response.fresp[0, 0, :]
+Hc =  compensated_response.fresp[0, 0, :]
 
-ax1.plot(GH_response.real.flatten(), GH_response.imag.flatten(), label='Uncompensated', color='blue')
-ax1.plot(compensated_response.real.flatten(), compensated_response.imag.flatten(), label='Compensated', color='orange')
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+ax1.plot(H.real.flatten(), H.imag.flatten(), label='Uncompensated', color='blue')
+ax1.plot(Hc.real.flatten(), Hc.imag.flatten(), label='Compensated', color='orange')
 theta = numpy.linspace(0, 2 * numpy.pi, 500)
 ax1.plot(numpy.cos(theta), numpy.sin(theta), '--', color='gray', label='Unit Circle')
 ax1.axhline(0, color='gray', linestyle='--')
 ax1.axvline(0, color='gray', linestyle='--')
+ax1.set_xlim(-10, 1)
+ax1.set_ylim(-10, 1)
 ax1.set_title("Full Nyquist Plot")
 ax1.set_xlabel("Re")
 ax1.set_ylabel("Im")
 ax1.legend()
 ax1.grid(True)
-ax1.axis("equal")
+#ax1.axis("equal")
 
-ax2.plot(GH_response.real.flatten(), GH_response.imag.flatten(), label="Uncompensated", color='blue')
-ax2.plot(compensated_response.real.flatten(), compensated_response.imag.flatten(), label='Compensated', color='orange')
+ax2.plot(H.real.flatten(), H.imag.flatten(), label="Uncompensated", color='blue')
+ax2.plot(Hc.real.flatten(), Hc.imag.flatten(), label='Compensated', color='orange')
 ax2.plot(numpy.cos(theta), numpy.sin(theta), '--', color='gray', label='Unit Circle')
 ax2.axhline(0, color='gray', linestyle='--')
 ax2.axvline(0, color='gray', linestyle='--')
@@ -47,7 +52,7 @@ ax2.set_xlabel('Re')
 ax2.set_ylabel('Im')
 ax2.legend()
 ax2.grid(True)
-ax2.axis('equal')
+#ax2.axis('equal')
 
-plt.tight_layout
+plt.tight_layout()
 plt.show()
