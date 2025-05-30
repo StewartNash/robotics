8d8f13a0ff7c58e11fc0d4093da6805feb8d15c7
StewartNash <Stewart.M.Nash@gmail.com>
2025-05-30 05:22:07 -0700

    Added signal processing section to manual

---
+++ signals/examples_3_2_1_kaiser.py
@@ -0,0 +1,7 @@
+# Kaiser order (NK) calculation
+# Type: 1 - lowpass, 2 - highpass, 3 - bandpass, 4 - bandstop
+# Passbands: FP1, FP2
+# Stopbands: FA1, FA2
+# Sampling rate: F
+# AP in dB, AA in dB
+# see design procedure section 5.3.2 steps 1 - 5
